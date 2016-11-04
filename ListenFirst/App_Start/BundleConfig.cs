7fdfa8ff2e46f458e843f5cead1b40f19c16a0e1
matthewharper <matthew.harper@yahoo.com>
2016-11-03 22:20:45 -0400

    mjh: Brought TemplateMonster into AdminLTE Mvc and got all path issues squared away.

--- ListenFirst/App_Start/BundleConfig.cs
+++ ListenFirst/App_Start/BundleConfig.cs
@@ -6,15 +6,42 @@ namespace ListenFirst.App_Start
 	{
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new StyleBundle("~/Bundles/css")
+
+			#region Shared Bundles. CorpSite and Dash.
+			bundles.Add(new ScriptBundle("~/bundles/jsShared")
+				.Include("~/Content/js/plugins/jquery/jquery-2.2.4.js"));
+			#endregion
+
+
+			#region CorpSite Bundles.  Originally from TemplateMonster template.  I bundled them.  Need to update versions where needed.  
+			bundles.Add(new ScriptBundle("~/bundles/jsCorp").Include(
+						"~/site/js/jquery-migrate-1.2.1.js",
+						"~/site/js/jquery.equalheights.js",
+						"~/site/js/jquery.mobile.customized.min.js",
+						"~/site/js/camera.js",
+						"~/site/js/wow/wow.js",
+						"~/site/js/script.js",
+						"~/site/js/modal.js",
+						"~/site/js/TMForm.js"));
+
+			bundles.Add(new StyleBundle("~/bundles/cssCorp").Include(
+					  "~/site/css/grid.css",
+					  "~/site/css/camera.css",
+					  "~/site/css/contact-form.css",
+					  "~/site/css/animate.css",
+					  "~/site/css/style.css"));
+			#endregion
+
+
+			#region Non-CorpSite Bundles.  Dashboard Bundles.  
+			bundles.Add(new StyleBundle("~/bundles/cssDash")
 				.Include("~/Content/css/bootstrap.css")
 				.Include("~/Content/css/select2.css")
 				.Include("~/Content/css/datepicker3.css")
 				.Include("~/Content/css/AdminLTE.css")
 				.Include("~/Content/css/skins/skin-blue.css"));
 
-			bundles.Add(new ScriptBundle("~/Bundles/js")
-				.Include("~/Content/js/plugins/jquery/jquery-2.2.4.js")
+			bundles.Add(new ScriptBundle("~/bundles/jsDash")
 				.Include("~/Content/js/plugins/bootstrap/bootstrap.js")
 				.Include("~/Content/js/plugins/fastclick/fastclick.js")
 				.Include("~/Content/js/plugins/slimscroll/jquery.slimscroll.js")
@@ -24,9 +51,11 @@ namespace ListenFirst.App_Start
 				.Include("~/Content/js/plugins/icheck/icheck.js")
 				.Include("~/Content/js/app.js")
 				.Include("~/Content/js/init.js"));
+			#endregion
+
 
 #if DEBUG
-            BundleTable.EnableOptimizations = false;
+			BundleTable.EnableOptimizations = false;
 #else
 			BundleTable.EnableOptimizations = true;
 #endif
---
+++ ListenFirst/Controllers/CorpSiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListenFirst.Controllers
+{
+    public class CorpSiteController : Controller
+    {
+        // GET: CorpSite
+        public ActionResult Index()
+        {
+            return View("CompanyHome");
+        }
+    }
+}
